e00b03d69d16504d240d0abbaa50078f6b748fba
grazyfunk04 <kunalmehndi10@gmail.com>
2023-12-24 17:09:30 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -4,19 +4,9 @@ const path = require('path')
 const hbs = require('hbs')
 const collection = require('./mongodb')
 
-
-const tempelatePath = path.join(__dirname, '../tempelates')
-const publicPath = path.join(__dirname, '../public')
-
 app.use(express.json())
 
-app.set('view engine', 'hbs')
-app.set('views', tempelatePath)
-
 app.use(express.urlencoded({extended:false}))
-
-app.use(express.static(publicPath))
-
 
 app.get("/contact_me", (req, res) => {
     res.render("contact_me")
@@ -36,9 +26,6 @@ app.post("/contact_me",async (req, res)=>{
 
 })
 
-
-
-
-app.listen(5000, () => {
+app.listen(8000, () => {
     console.log("Port Connected");
 })
